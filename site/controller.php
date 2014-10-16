e34f9989e9f4b0c558ad3136cb0b74c75b64d707
Dax Panganiban <dax1216@gmail.com>
2014-10-17 00:20:23 +0800

    TESTING OUT JOOMLA COMPONTENT

--- site/controller.php
+++ site/controller.php
@@ -18,6 +18,64 @@ jimport('joomla.application.component.controller');
  *
  * @since   0.0.1
  */
+
+define('RATE_URL', 'http://www.emmi-benchmarks.eu/euribor-eonia-org/about-eonia.html');
+
+set_time_limit(0);
+
 class DebtTickerController extends JControllerLegacy
 {
+
+    public function set_rate() {
+        $contents = $this->_make_request(RATE_URL);
+
+        preg_match("/<br\/>(-?[0-9]\d*(\.\d+))<br\/>/i", $contents, $rate_matches);                
+        preg_match("/<br\/> \((\d{2}\/\d{2}\/\d{4})\)/i", $contents, $rate_date_matches);
+        
+        if(!empty($rate_matches) && !empty($rate_date_matches) && !empty($rate_matches[1]) && !empty($rate_date_matches[1])) {
+           
+            
+        }
+    }
+    
+    public function get_rate() {
+        
+    }
+    
+    private function _make_request($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL,            $url);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT,      'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12');
+        curl_setopt($curl, CURLOPT_HEADER,         0);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return $result;
+     }
+
+    private function _send_email() {
+        $mailer = JFactory::getMailer();
+        
+        $config = JFactory::getConfig();
+        $sender = array( 
+            $config->getValue( 'config.mailfrom' ),
+            $config->getValue( 'config.fromname' ) );
+
+        $mailer->setSender($sender);
+        
+        $mailer->addRecipient($config->getValue( 'config.mailfrom' ));
+        
+        $body = "Component unable to retrieve interest rate from http://www.emmi-benchmarks.eu/euribor-eonia-org/about-eonia.html. Please contact developer for help.";
+        $mailer->setSubject('Joomla Debt Ticker notification');
+        $mailer->setBody($body);
+        
+        $send = $mailer->Send();
+        if ( $send !== true ) {
+            echo 'Error sending email: ' . $send->__toString();
+        } else {
+            echo 'Mail sent';
+        }
+    }
 }
--- site/models/debtticker.php
+++ site/models/debtticker.php
@@ -10,34 +10,47 @@ jimport('joomla.application.component.modelitem');
  */
 class DebtTickerModelDebtTicker extends JModelItem
 {
-   /**
-   * @var string msg
-   */
-   protected $textBefore;
-   
-   protected $textAfter;
+    /**
+    * @var string msg
+    */
+    protected $textBefore;
 
-   /**
-   * Get the message
-   * @return string The message to be displayed to the user
-   */
-   public function getTextBefore() 
-   {
-      if (!isset($this->textBefore)) 
-      {
-         $this->textBefore = 'Hello World!';
-      }
-      
-      return $this->textBefore;
-   }
+    protected $textAfter;
+
+    /**
+    * Get the message
+    * @return string The message to be displayed to the user
+    */
+    public function getTextBefore() 
+    {
+       if (!isset($this->textBefore)) 
+       {
+          $this->textBefore = 'Hello World!';
+       }
+
+       return $this->textBefore;
+    }
    
-   public function getTextAfter() 
-   {
-      if (!isset($this->textAfter)) 
-      {
-         $this->textBefore = 'Bye World!';
-      }
-      
-      return $this->textAfter;
-   }
+    public function getTextAfter() 
+    {
+       if (!isset($this->textAfter)) 
+       {
+          $this->textBefore = 'Bye World!';
+       }
+
+       return $this->textAfter;
+    }
+   
+    public function getSettings() 
+    {
+        $db = JFactory::getDBO();
+        $query = $db->getQuery(true);
+        $query->select('*');
+        $query->from('#__debtticker');
+        $query->where($db->quoteName('id')." = ".$db->quote(1));
+        $db->setQuery((string)$query);
+        $options = $db->loadRow();
+        
+        return $settings;
+    }
 }
