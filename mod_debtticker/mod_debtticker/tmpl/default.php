03a2674e655fd583934ba5499e63c016b88a43d0
Dax Panganiban <dax1216@gmail.com>
2014-10-20 00:58:45 +0800

    Liability ticker

--- mod_debtticker/mod_debtticker/tmpl/default.php
+++ mod_debtticker/mod_debtticker/tmpl/default.php
@@ -3,22 +3,22 @@
 defined('_JEXEC') or die;
 
 $doc = JFactory::getDocument();
-$doc->addStyleSheet( JUri::root() . 'modules/mod_debtticker/css/odometer.css' );
-$doc->addScript( JUri::root() . 'modules/mod_debtticker/js/odometer.js' );
+$doc->addStyleSheet( JUri::root() . '/media/mod_debtticker/css/odometer.css' );
+$doc->addScript( JUri::root() . '/media/mod_debtticker/js/odometer.js' );
 
 ?>
 <div class="dt-mod">
     <div class="dt-text-before"><?= $settings['text_before'] ?></div>
-    <div id="dt-display"></div>
+    <div id="dt-display" class="odometer"><?=$comDebtTicker['current_liabilities']?></div>
     <div class="dt-text-before"><?= $settings['text_after'] ?></div>
 </div>
 
-<script type="text/javscript">
-   var el = document.getElementById('dt-display');
-
+<script type="text/javascript">
+   var elmdt = document.getElementById('dt-display');
+   
    var od = new Odometer({
-     el: el,
-     value: 333555,
+     el: elmdt,
+     value: <?=$comDebtTicker['current_liabilities']?>,
 
      // Any option (other than auto and selector) can be passed in here
      format: '(,ddd)',
@@ -26,11 +26,12 @@ $doc->addScript( JUri::root() . 'modules/mod_debtticker/js/odometer.js' );
    });
    var seconds = <?= $seconds?>;
    
-   setInterval( function() {
-      var debt_value = 400000000 * (1 + <?=$recentRate['rate']?> * ( seconds / 31104000));
-      seconds += 1000;
-      od.update(debt_value);
-   }, 1000);
+   var dttimer = setInterval( function() { setDebtValue() }, 3000);
    
+   function setDebtValue() {
+      debt_value = 400000000 * (1 + <?=$recentRate['rate']?> * ( seconds / 31104000));
+      seconds += 3000;
+      od.update(debt_value);      
+   }
    
 </script>
