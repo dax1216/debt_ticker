e417dece559a898defeede480c4a89ef50788bea
Dax Panganiban <p_dax2003@yahoo.com>
2014-11-05 18:20:42 +0800

    Mods to the frontend ticker

--- mod_debtticker/mod_debtticker/tmpl/default.php
+++ mod_debtticker/mod_debtticker/tmpl/default.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // No direct access
 defined('_JEXEC') or die;
 
@@ -13,26 +13,25 @@ $doc->addScript( JUri::root() . '/media/mod_debtticker/js/odometer.js' );
     <div class="dt-text-before"><?= $settings['text_after'] ?></div>
 </div>
 <input type="hidden" id="debt" value="<?=$recentRate['debt']?>" />
+<input type="hidden" value="<?=date('Y-m-d H:i:s')?>" />
 <script type="text/javascript">
    var elmdt = document.getElementById('dt-display');
-   
+
    var od = new Odometer({
      el: elmdt,
      value: <?=$recentRate['debt']?>,
-     duration: 1000,
      // Any option (other than auto and selector) can be passed in here
      format: '(,ddd)',
      theme: 'train-station',
-     animation: 'count',
      auto: true
    });
-   
-   var dttimer = setInterval( function() { setDebtValue() }, 2000);
-   
-   function setDebtValue() {       
-      var debt = Number(document.getElementById('debt').value) + 1;
-      od.update(debt);
-      document.getElementById('debt').value = debt;
+
+   var dttimer = setInterval( function() { setDebtValue() }, 3000);
+   var debt = <?=$recentRate['debt']?>;
+
+   function setDebtValue() {
+      od.update(debt++);
    }
-   
+
+
 </script>
